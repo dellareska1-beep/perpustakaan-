998d61f6fc85668bc9e3b7053ca915376d5e5098
Della Reska <dellareska1@gmail.com>
2026-03-12 00:16:32 +0700

    update readme

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,3 @@
+<div>
+    Silakan hubungi kami melalui contact berikut untuk informasi lebih lanjut atau bantuan terkait layanan perpustakaan digital kami. Kami siap membantu Anda dengan pertanyaan atau kebutuhan Anda.
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<div>
+Selamat Datang di Sistem Perpustakaan Digital {{$nama}}<br>
+Anda adalah seorang {{$pekerjaan}}
+</div>
